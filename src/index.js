de990f6235304cf1804c556d392254e5e49d782d
Raa0404 <piyushrana007.pr@gmail.com>
2025-07-08 19:42:13 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div>OTS Final App - All Phases Functional</div>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
